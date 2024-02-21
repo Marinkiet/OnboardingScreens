89c4cf97a78972f0bb6be76d467a1b84a54e4a4e
Marinkie <tmarinkie12@gmail.com>
2024-02-21 02:42:00 +0200

    Fix: indentation on the view

--- src/components/Buttons/FilledButton.js
+++ src/components/Buttons/FilledButton.js
@@ -1,12 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 
-
-
-
 const FilledButton = (props) => {
   return (
-    <View 
-    style={[styles.button,{width:props.btnWidth||60,height:props.btnHeight ||50}]}><Text style={styles.buttonText}>{props.text}</Text>
+    <View
+      style={[styles.button, { width: props.btnWidth || 60, height: props.btnHeight || 50 }]}>
+      <Text style={styles.buttonText}>{props.text}</Text>
     </View>
   )
 }
@@ -14,14 +12,14 @@ const FilledButton = (props) => {
 export default FilledButton
 
 const styles = StyleSheet.create({
-  button:{
-    alignItems:'center',
-    justifyContent:'center',
-    backgroundColor:'#ffd503',
-    borderRadius:40
-},
-buttonText:{
-    fontSize:20,
-    color:"#07101e",
-},
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffd503',
+    borderRadius: 40
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#07101e",
+  },
 })
